0924b242b0b7846a5144383caf1acf9240252a65
Sudharsan-Spark <sudharsankamalakkannan@gmail.com>
2024-04-16 23:23:36 +0530

    updated lambda script

--- ngos_zios_generate_report.py
+++ ngos_zios_generate_report.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.INFO)
 s3client = boto3.client('s3')
 DATE = "%s-%02d-%02d" %(datetime.now().year, datetime.now().month, datetime.now().day)
 
-#checker
+#change
 
 def lambda_handler(event, context):
     bucket = "zadarastorage-metering"
